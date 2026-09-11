287ce8fab60d3a2acdfdd5ed45b6cf4e4eb63f46
wearixastore@gmail.com <detective0114@outlook.com>
2026-09-12 02:55:13 +0500

    this fixdouble

--- server/server.js
+++ server/server.js
@@ -35,8 +35,8 @@ app.use(cors({
   origin: "*",
   credentials: true
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Serve uploaded avatars and media statically
 app.use("/uploads", express.static(path.join(__dirname, "data/uploads")));
